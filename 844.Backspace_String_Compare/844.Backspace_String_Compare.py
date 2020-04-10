1f03903e9d884268becd1e9d94f2fd3cf27c2ae8
Ather <atherkhalid158@gmail.com>
2020-04-10 22:16:26 +0200

    155 solution + 844 fix

--- 844.Backspace_String_Compare/844.Backspace_String_Compare.py
+++ 844.Backspace_String_Compare/844.Backspace_String_Compare.py
@@ -5,25 +5,23 @@ class Solution(object):
         :type T: str
         :rtype: bool
         """
-        
+
         stackS = []
         stackT = []
-        
+
         for char in S:
             if(char != '#'):
                 stackS.append(char)
-            elif (len(stackS)>0):
+            elif (len(stackS) > 0):
                 stackS.pop()
-                
+
         for char in T:
             if(char != '#'):
                 stackT.append(char)
-            elif (len(stackT)>0):
+            elif (len(stackT) > 0):
                 stackT.pop()
-                
+
         if(stackT == stackS):
             return True
         else:
             return False
-                
-      
