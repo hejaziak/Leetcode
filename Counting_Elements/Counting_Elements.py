b308c18f71f274ad25ec1178be82f6d15e537eb4
Ather <atherkhalid158@gmail.com>
2020-04-12 22:34:55 +0200

    solving Counting Elements

---
+++ Counting_Elements/Counting_Elements.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def countElements(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        count = 0
+        for el in arr:
+            if((el + 1) in arr):
+                count += 1
+        return count
